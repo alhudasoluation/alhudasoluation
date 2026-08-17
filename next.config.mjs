b447cc892f8066da8a00a42e34ff506bf065298e
alhudasoluation <alhudasoluation@gmail.com>
2026-08-17 21:28:57 +0600

    publish 1.0.1

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  output: "export",
+  basePath: "/alhudasoluation",
+  assetPrefix: "/alhudasoluation",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
